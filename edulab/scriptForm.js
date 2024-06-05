6920427fe364464a9101546b54d07044cc2bc1a7
Alexander <ja.gaslacma@unsm.edu.pe>
2024-06-05 09:11:01 -0500

    2 commit

---
+++ edulab/scriptForm.js
@@ -0,0 +1,18 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+  
+    // Get values from form
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  
+    // Simple validation
+    if (username === 'usuario' && password === 'contraseña') {
+      document.getElementById('message').innerHTML = 'Inicio de sesión exitoso';
+      window.location.href = 'index.html';
+
+
+    } else {
+      document.getElementById('message').innerHTML = 'Credenciales inválidas';
+    }
+  });
+  
